75848f182674f56cfdfbed36af2bba284189bfc5
Alberto La Rocca <a71104@gmail.com>
2016-04-22 12:38:56 +0100

    Quine

---
+++ quine.js
@@ -0,0 +1,1 @@
+var q='\'',s='\\';(function(x){console.log("var q="+q+s+q+q+",s="+q+s+s+q+";("+x+"("+q+x+q+"));")}('function(x){console.log("var q="+q+s+q+q+",s="+q+s+s+q+";("+x+"("+q+x+q+"));")}'));
